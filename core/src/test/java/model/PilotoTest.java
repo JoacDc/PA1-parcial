c4da41beeac82970dc79b81e8aec296915d822d0
JoacDc <delcantodiegojoaquin5@gmail.com>
2025-10-14 17:34:18 -0300

    Primer commmit para saber que esta todo ok

---
+++ core/src/test/java/model/PilotoTest.java
@@ -0,0 +1,18 @@
+package model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+public class PilotoTest {
+
+
+    @Test
+    @Order(1)
+    @DisplayName("CAMINO FELIZ ")
+
+    public void camino_feliz_test(){
+
+    }
+
+}
---
+++ core/src/test/java/usecase/CrearPilotoUseCaseTest.java
@@ -0,0 +1,14 @@
+package usecase;
+
+import org.junit.jupiter.api.Test;
+
+public class CrearPilotoUseCaseTest {
+
+    @Test
+    public void crear_pilot_test(){
+
+    }
+
+}
+
+
